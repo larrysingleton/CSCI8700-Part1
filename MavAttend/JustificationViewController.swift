dc4e9f23dcdcc1785f90cf1268b9c581b3ad13f4
Ian Vaughn Koeppe <ianvkoeppe@icloud.com>
2016-10-15 08:48:57 -0500

    Create New View Controller for Disputing Absence

---
+++ MavAttend/JustificationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  JustificationViewController.swift
+//  MavAttend
+//
+//  Created by Ian Vaughn Koeppe on 10/15/16.
+//  Copyright © 2016 Ian Vaughn Koeppe. All rights reserved.
+//
+
+import UIKit
+
+class JustificationViewController: UIViewController {
+
+}
