63eeedccf2f50f1ceac9d10493ef3e67ecdbf19d
Ian Vaughn Koeppe <ianvkoeppe@icloud.com>
2016-10-15 16:15:11 -0500

    Finish Dispute Workflow

--- MavAttend/JustificationViewController.swift
+++ MavAttend/JustificationViewController.swift
@@ -21,15 +21,26 @@ class JustificationViewController: UIViewController {
         label.text = "Reason for being tardy/absent:"
         self.view.addSubview(label)
         
-        let justification = UITextField(frame: CGRect(x: 5, y: 105, width: self.view.bounds.width - 10, height: self.view.bounds.height - 140))
+        let justification = UITextField(frame: CGRect(x: 5, y: 105, width: self.view.bounds.width - 10, height: self.view.bounds.height - 540))
         justification.backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1.0)
         justification.contentVerticalAlignment = UIControlContentVerticalAlignment.top;
         self.view.addSubview(justification)
         
-        let button = UIButton(type: UIButtonType.roundedRect)
-        button.frame = CGRect(x: 5, y: 5, width: self.view.bounds.width - 10, height: 25)
-        button.titleLabel!.text = "Submit"
-        justification.inputView?.addSubview(button)
+        let button = UIButton(type: UIButtonType.system)
+        button.addTarget(self, action: #selector(pressed), for: .touchUpInside)
+        button.frame = CGRect(x: 5, y: 665, width: self.view.bounds.width - 10, height: 60)
+        button.backgroundColor = UIColor(red: 0, green: 255, blue: 0, alpha: 1.0)
+        button.setTitleColor(UIColor(red: 255, green: 255, blue: 255, alpha: 1.0), for: .normal)
+        button.setTitle("Submit", for: .normal)
+        self.view.addSubview(button)
+    }
+    
+    func pressed(sender: UIButton) {
+        let alert = UIAlertController(title: "Submitted!", message: "Your professor will review your dispute.", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {(action: UIAlertAction!) in}))
+        
+        self.navigationController!.popViewController(animated: true)
+        self.navigationController!.present(alert, animated: true, completion: nil)
     }
     
 }
